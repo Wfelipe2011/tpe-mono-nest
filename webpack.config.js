32c0754b1b2723d2dc9165bb39fdc516973e050c
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2024-07-11 07:19:14 -0300

    feat: configure deployment with Serverless Framework
    Implements deployment configuration using the Serverless Framework, enabling efficient management and deployment of serverless functions.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const TerserPlugin = require('terser-webpack-plugin');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 
 module.exports = (options, webpack) => {
   const lazyImports = [
     '@nestjs/microservices/microservices-module',
     '@nestjs/websockets/socket-module',
   ];
+
+  const outDir = options.output.filename.split('/').slice(0, -1).join('/');
+  const filename = options.output.filename.split('/').pop();
 
   return {
     ...options,
@@ -27,6 +32,8 @@ module.exports = (options, webpack) => {
     ],
     output: {
       ...options.output,
+      path: path.resolve(__dirname, outDir, 'dist'),
+      filename,
       libraryTarget: 'commonjs2',
     },
     optimization: {
