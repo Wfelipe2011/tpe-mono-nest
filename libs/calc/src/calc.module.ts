aaf244c5af45c5ddd5007623709eec2dca4db471
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2024-08-23 23:23:17 -0300

    chore: Remove my-app-1 and my-app-2 apps and related files

--- libs/calc/src/calc.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CalcService } from './calc.service';
-
-@Module({
-  providers: [CalcService],
-  exports: [CalcService],
-})
-export class CalcModule {}
